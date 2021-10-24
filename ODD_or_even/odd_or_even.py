8c8de95a7d73a94e2b0edd58aeba0b6add266ae6
iftun <72260688+iftuun@users.noreply.github.com>
2021-10-25 03:45:21 +0600

    Add files via upload

---
+++ ODD_or_even/odd_or_even.py
@@ -0,0 +1,7 @@
+print("welcome to my even odd calculator.....")
+
+n=int(input("please enter a number : "))
+if n%2==0:
+    print("this is an even number")
+else:
+    print("this is an odd number")
